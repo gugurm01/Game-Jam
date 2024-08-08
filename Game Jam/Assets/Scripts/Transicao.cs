310400d321b4982f9fc494807ed33cb1f44c95dc
BelledeJourr <freireisabelle12@gmail.com>
2024-08-08 16:16:05 -0300

    deu certo

--- Game Jam/Assets/Scripts/Transicao.cs
+++ Game Jam/Assets/Scripts/Transicao.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Transicao : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    public string nomeDaProximaCena; // Nome da cena que você deseja carregar
+
+    
+    public void CarregarProximaCena()
     {
-        
+        SceneManager.LoadScene(nomeDaProximaCena);
     }
 }
+
